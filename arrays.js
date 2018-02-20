385d4f2b2a23d57185916caa49c6e8978b33e0ae
Richard Fenix <richard.fenix@gmail.com>
2018-02-20 04:57:14 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,59 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(choc, moreChoc) {
+  let newList = [moreChoc, ...choc];
+  return newList;
+}
+
+function destructivelyAddElementToBeginningOfArray(choc, moreChoc) {
+  choc.unshift(moreChoc);
+  return choc;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(choc, moreChoc) {
+  choc.push(moreChoc);
+  return choc;
+}
+
+function accessElementInArray(choc, index) {
+  return choc[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(choc) {
+  choc.shift();
+  return choc;
+}
+
+var results = destructivelyRemoveElementFromBeginningOfArray(chocolateBars);
+console.log(results);
+
+function removeElementFromBeginningOfArray(choc){
+  return choc.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(choc){
+  return choc.pop();
+}
+
+var results = destructivelyRemoveElementFromBeginningOfArray(chocolateBars);
+console.log(results);
+
+
+function removeElementFromEndOfArray(choc) {
+  return choc.slice(0, choc.length - 1);
+}
+
+
+// addElementToBeginningOfArray(chocolateBars, "milkyway");
+
+// destructivelyAddElementToBeginningOfArray(chocolateBars, "milkyway");
+
+// addElementToEndOfArray(chocolateBars, "kisses");
+
+// destructivelyAddElementToEndOfArray(chocolateBars, "kisses");
+
+
